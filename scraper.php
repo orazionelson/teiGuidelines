c31b301a5e21f0ae268ff7419ea649738b5f7afc
Alfredo Cosco <alfredo.cosco@gmail.com>
2019-10-03 15:28:25 +0200

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,5 +1,13 @@
 <?php
 
-echo "scraper";
+// This is a template for a PHP scraper on morph.io (https://morph.io)
+// including some code snippets below that you should find helpful
+ require 'scraperwiki.php';
+ require 'scraperwiki/simple_html_dom.php';
+//
+// // Read in a page
+$html = scraperwiki::scrape("https://tei-c.org/guidelines/P5/");
+
+var_dump($html);
 
 ?>
